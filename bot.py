b198520ced1539b4953c91a3e3c3e072deac7287
Niunicki <ram.povilaitis@gmail.com>
2018-10-26 21:44:59 +0100

    added .whois

--- bot.py
+++ bot.py
@@ -16,6 +16,23 @@ async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name='DevSoc')
     await bot.add_roles(member, role)
 
+#Commands
+@Bot.command
+async def whois(ctx, user):
+    user_info = discord.Embed(title = "Name", description=user.name, color=user.color)
+    user_info.set_thumbnail(user.avatar_url)
+    user_info.set_author(name=user.name + '#' + user.discriminator, icon_url= user.avatar_url)
+    user_info.add_field(name='Nickname', value=user.nick)
+    user_info.add_field(name='Status', value=user.status)
+    user_info.add_field(name='Joined Server', value=user.joined_at)
+    user_info.add_field(name='Joined Discord', value=user.created_at)
+    user_roles = []
+    for i in range(len(user.roles)):
+        user_roles.append(user.roles[i].mention)
+    user_info.add_field(name='Roles', value=', '.join(user_roles))
+    user_info.set_footer(text='ID: ' + user.id)
+    await ctx.send(user_info)
+
 
 @bot.event
 async def on_ready():
