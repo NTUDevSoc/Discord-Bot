61b1d1a30000ffad402fed31cd48b4f481be98ae
Niunicki <ram.povilaitis@gmail.com>
2018-10-26 23:36:20 +0100

    finished .whois

--- bot.py
+++ bot.py
@@ -19,20 +19,28 @@ async def on_member_join(member):
 
 #Commands
 @bot.command()
-async def whois(channel, user:discord.Member):
-    user_info = discord.Embed(title = "Name", description=user.name, color=user.color)
-    user_info.set_thumbnail(url=user.avatar_url)
-    user_info.set_author(name=user.name + '#' + user.discriminator, icon_url= user.avatar_url)
-    user_info.add_field(name='Nickname', value=user.nick)
-    user_info.add_field(name='Status', value=user.status)
-    user_info.add_field(name='Joined Server', value=user.joined_at)
-    user_info.add_field(name='Joined Discord', value=user.created_at)
-    user_roles = []
-    for i in range(len(user.roles)):
-        user_roles.append(user.roles[i].mention)
-    user_info.add_field(name='Roles', value=', '.join(user_roles))
-    user_info.set_footer(text='ID: ' + str(user.id))
-    await channel.send(embed=user_info)
+async def whois(channel, user:discord.Member=None):
+    if user:
+        user_info = discord.Embed(title = "Name", description=user.name, color=user.color)
+        user_info.set_thumbnail(url=user.avatar_url)
+        user_info.set_author(name=user.name + '#' + user.discriminator, icon_url= user.avatar_url)
+        user_info.add_field(name='Nickname', value=user.nick)
+        user_info.add_field(name='Status', value=user.status)
+        user_info.add_field(name='Joined Server', value=user.joined_at)
+        user_info.add_field(name='Joined Discord', value=user.created_at)
+        user_roles = []
+        for i in range(len(user.roles)):
+            user_roles.append(user.roles[i].mention)
+        user_info.add_field(name='Roles', value=', '.join(user_roles))
+        user_info.set_footer(text='ID: ' + str(user.id))
+        await channel.send(embed=user_info)
+    else:
+        await channel.send('Please tag an user!')
+@whois.error
+async def whois_error(channel, error):
+    if isinstance(error, discord.ext.commands.BadArgument):
+        await channel.send('Please tag an user!')
+
 
 #function to make the bot print every 28mins so Heroku doesn't stop it
 async def stay_awake():
