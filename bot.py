4dbbebea64fd54ac99cc521bc9958d528f60745f
Niunicki <ram.povilaitis@gmail.com>
2018-10-28 01:47:46 +0100

    seperated commands from main

--- bot.py
+++ bot.py
@@ -1,50 +1,21 @@
 #by Ram :)
 
 #imports
-import os
-import asyncio
-import discord
+import os, asyncio, discord
 from discord.ext import commands
 
 #global vars
 TOKEN = os.environ['DISCORD_TOKEN']
 
-#set up bot object
+#set up bot object and cogs
 bot = commands.Bot(command_prefix='.')
+extensions = ['commands']
 
+#EVENTS
 @bot.event
 async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name='DevSoc')
     await bot.add_roles(member, role)
-
-#Commands
-@bot.command()
-async def whois(ctx, user:discord.Member=None):
-    if user:
-        user_info = discord.Embed(title = "Name", description=user.name, color=user.color)
-        user_info.set_thumbnail(url=user.avatar_url)
-        user_info.set_author(name=user.name + '#' + user.discriminator, icon_url= user.avatar_url)
-        user_info.add_field(name='Nickname', value=user.nick)
-        user_info.add_field(name='Status', value=user.status)
-        user_info.add_field(name='Joined Server', value=user.joined_at.strftime("%Y-%m-%d %H:%M:%S"))
-        user_info.add_field(name='Joined Discord', value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"))
-        user_roles = []
-        for i in range(len(user.roles)):
-            user_roles.append(user.roles[i].mention)
-        user_info.add_field(name='Roles', value=', '.join(user_roles))
-        user_info.set_footer(text='ID: ' + str(user.id))
-        await ctx.send(embed=user_info)
-    else:
-        await ctx.send('Please tag an user!')
-@whois.error
-async def whois_error(channel, error):
-    if isinstance(error, discord.ext.commands.BadArgument):
-        await channel.send('Please tag an user!')
-
-#Command that links to GitHub
-@bot.command()
-async def github(ctx):
-    await ctx.send("You can find the source code on: https://github.com/NTUDevSoc/Discord-Bot")
 
 #function to make the bot print every 28mins so Heroku doesn't stop it
 async def stay_awake():
@@ -52,6 +23,14 @@ async def stay_awake():
     while True:
         print('Im awake :)')
         await asyncio.sleep(1680)
+
+#Load our extensions(cogs)
+if __name__ == "__main__":
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+        except:
+            print('Failed to load extension ' + extension + '.')
 
 @bot.event
 async def on_ready():
