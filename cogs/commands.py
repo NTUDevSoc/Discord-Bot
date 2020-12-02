d9542840d88457dc7735aef7fa330efe3d9fefed
Peter <34271471+petelampy@users.noreply.github.com>
2020-12-02 12:18:02 +0000

    reworked the stars command

--- cogs/commands.py
+++ cogs/commands.py
@@ -267,10 +267,8 @@ class Commands(commands.Cog):
             adventCache = json.loads(data)
         dataArray = []
         for member in adventCache["members"]:
-            counter = 0
-            for x in adventCache["members"][member]["name"]["completion_day_level"][str(day)]:
-                counter += 1
-            userTuple = (adventCache["members"][member]["name"], counter)
+            stars = len(adventCache["members"][member]["name"]["completion_day_level"][str(day)])
+            userTuple = (adventCache["members"][member]["name"], stars)
             dataArray.append(userTuple)
         dataArray.sort(key=lambda tup: tup[1], reverse=True)
         advent=discord.Embed(title="__Advent of Code - Day "+day+" Stars__", color=0xe7ec11)
